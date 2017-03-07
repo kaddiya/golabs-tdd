821817c89dc984cb8b95dc2955bf056412140555
kaddiya <kaddiya@gmail.com>
2017-03-07 19:22:09 +0530

    Revert "Pass multiplication"

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -4,14 +4,3 @@ package calculator
 func Add(a, b int) int {
 	return a + b
 }
-
-//Subtract performs the subtraction of two numbers
-func Subtract(a, b int) int {
-	return a - b
-}
-
-//Multiply performs multiplication of two numbers
-func Multiply(a, b int) int {
-	return a * b
-
-}
