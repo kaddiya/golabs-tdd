62a26f338d3b58ae9ea41cc0bed2c83a6d11ef5e
Ameya <ameya.kulkarni@weboniselab.com>
2017-03-07 18:34:01 +0530

    Added the suite

---
+++ calculator/calculator_test.go
@@ -0,0 +1,19 @@
+package calculator
+
+import "testing"
+
+func TestCanAdd2Numbers(t *testing.T) {
+
+}
+
+func TestCanSubtract2Numbers(t *testing.T) {
+
+}
+
+func TestCanMultiple2Numbers(t *testing.T) {
+
+}
+
+func TestCanDivide2Numbers(t *testing.T) {
+
+}
