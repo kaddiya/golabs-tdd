0996857440c1fa9b2c387b56e2bc2ff52b47f464
Ameya <ameya.kulkarni@weboniselab.com>
2017-03-15 15:03:27 +0530

    Refactored tio table driven tests

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -1,6 +1,9 @@
 package calculator
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestCanAdd2Numbers(t *testing.T) {
 	result := Add(5, 6)
@@ -17,19 +20,31 @@ func TestCanMultiple2Numbers(t *testing.T) {
 	logAndAssert(90, result, "Multiplication of 10 and 9 should yield 90 but yielded ", t)
 }
 
-func TestCanDivide2Numbers(t *testing.T) {
-	result, _ := Divide(90, 9)
-	logAndAssert(10, result, "Division of 90 by 9 should yield 10 byt yielded", t)
+var divideTests = []struct {
+	dividend int
+	divisor  int
+	result   int
+	message  string
+	err      error
+}{
+	{90, 9, 10, "Division of 90 by 9 doesnt not yield 10", nil},
+	{12, 4, 3, "Division of 12 by 4 doesnt not yield 3", nil},
+	{45, 7, 6, "Division of 45 by 7 doesnt not yield a round number 6", nil},
+	{0, 12, 0, "Division of 0 by 12 should give 0 and a nil error", nil},
+	{45, 0, 0, "Divisor by 0 should not be allowed", errors.New("Divisor cant be equal to 0")},
 }
 
-func TestDivisionBy0ShouldNotBeAllowed(t *testing.T) {
-	_, err := Divide(10, 0)
-
-	if err == nil || "Divisor cant be equal to 0" != err.Error() {
-		t.Log("Incorrect error message found for division by 0")
-		t.Fail()
+func TestDivide(t *testing.T) {
+	for _, tuple := range divideTests {
+		actual, err := Divide(tuple.dividend, tuple.divisor)
+		if err != nil {
+			if err.Error() != tuple.err.Error() {
+				t.Fail()
+				t.Log("expected error was " + tuple.err.Error() + " but got " + err.Error())
+			}
+		}
+		logAndAssert(tuple.result, actual, tuple.message, t)
 	}
-
 }
 
 func logAndAssert(expected, actual int, message string, t *testing.T) {
