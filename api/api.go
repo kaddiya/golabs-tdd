4a0727d5e64ff3d8bacef307df9971b96ae9d630
Ameya <ameya.kulkarni@weboniselab.com>
2017-03-20 12:46:06 +0530

    PAssed validations for invalid Email and short passwrod

--- api/api.go
+++ api/api.go
@@ -1,26 +1,51 @@
 package api
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 //UserSignUpRequest encapuslates the signup request
 type UserSignUpRequest struct {
-	userName string
-	email    string
-	password string
+	UserName string
+	Email    string
+	Password string
 }
 
 //UserSignUpResponse encapsulates the Signup response
 type UserSignUpResponse struct {
-	userName string
-	userID   int
+	UserName string
+	UserID   int
 }
+
+type user struct {
+	UserID   int
+	UserName string
+	Email    string
+	Password string
+}
+
+const userIDValue = 0
 
 //SignUpUser exposes the signup API
 func SignUpUser(w http.ResponseWriter, r *http.Request) {
+	reqBody := &UserSignUpRequest{}
+	parseRequestBody(r.Body, reqBody)
+
+	//check for valid email address
+	if !strings.Contains(reqBody.Email, "@") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	//check for the length of the password
+	if len(reqBody.Password) < 8 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 }
 
@@ -30,3 +55,9 @@ func InitRouter() *mux.Router {
 	r.HandleFunc("/user/new", SignUpUser).Methods("POST")
 	return r
 }
+
+func parseRequestBody(r io.Reader, target interface{}) interface{} {
+	return json.NewDecoder(r).Decode(target)
+}
+
+//functions to populate the DataStore
